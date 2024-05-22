48259472c435c4d3956bcb39d5aa24168722bdbf
chauthutran <cthutran@gmail.com>
2024-05-22 09:48:26 +0900

    Updated

---
+++ app/dashboard/(overview)/page copy.tsx
@@ -0,0 +1,72 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+// import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+import styles from '@/app/ui/home.module.css';
+import AcmeLogo from '@/app/ui/acme-logo';
+import Image from 'next/image';
+
+
+export default async function Page() {
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
+
+  // const totalData = await fetchCardData();
+
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        {/* <AcmeLogo /> */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
+
+      {/* <div className={styles.shape} >fasdfads</div> */}
+
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      
+
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+        {/* <Card title="Collected" value={totalData.numberOfInvoices} type="collected" />
+        <Card title="Pending" value={totalData.totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={totalData.numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={totalData.numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+
+      </div>
+    </main>
+  );
+}
